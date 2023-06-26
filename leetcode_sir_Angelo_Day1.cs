83182cf36498f4126daed90c4fe214c8f75c31c6
stephen4018 <stephenohaeri4018@gmail.com>
2023-06-26 19:13:55 +0100

    added new files

--- leetcode_sir_Angelo_Day1.cs
+++ leetcode_sir_Angelo_Day1.cs
@@ -37,72 +37,14 @@ namespace MY_ALGORITHMS
         }
 
 
-        //You are a product manager and currently leading a team to develop a new product.Unfortunately, thelatest version of your product fails the quality check.Since each version is developed based on the previous version, all the versions after a bad version are also bad. Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one, which causes all the following ones to be bad. You are given an API bool isBadVersion(version) which returns whether version is bad. Implement a function to find the first bad version. You should minimize the number of calls to the API. Input: n = 5, bad = 4
-        //Output: 4
-        //Explanation:
-        //call isBadVersion(3) -> false
-        //call isBadVersion(5) -> true
-        //call isBadVersion(4) -> true
-        //Then 4 is the first bad version.
+       
 
-        public int FirstBadVersion(int n)
-        {
-            return VersionFunction(1, n);
-        }
+       
 
-        public static int VersionFunction(int first, int last)
-        {
-            while (first < last)
-            {
-                int median = first + (last - first) / 2;
-
-                if (IsBadVersion(median))
-                {
-                    last = median;
-                }
-                else
-                {
-                    first = median + 1;
-                }
-
-            }
-            return first;
-        }
-
-        public static bool IsBadVersion(int digit)
-        {
-            return true;
-        }
+      
 
 
-        //Given a sorted array of distinct integers and a target value, return the index if the target is found.If not, return the index where it would be if it were inserted in order.
-        //You must write an algorithm with O(log n) runtime complexity.
-        public int SearchInsert(int[] nums, int target)
-        {
-            int start = 0;
-            int end = nums.Length - 1;
-
-            while (start <= end)
-            {
-                int median = start + (end - start) / 2;
-
-                if (nums[median] == target)
-                {
-                    return median;
-                }
-                else if (nums[median] < target)
-                {
-                    start = median + 1;
-                }
-                else
-                {
-                    end = median - 1;
-                    // return end;
-                }
-
-            }
-            return start;
-
-        }
+      
+        
     }
 }
