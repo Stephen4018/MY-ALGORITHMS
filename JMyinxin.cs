bab393cf241c2fab79edc995985d89a607397153
stephen4018 <stephenohaeri4018@gmail.com>
2023-10-17 08:55:02 +0100

    question added

---
+++ JMyinxin.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MY_ALGORITHMS
+{
+    public class JMyinxin
+    {
+   //     A little boy is studying arithmetics.He has just learned how to add two integers, written one below another, column by column.But he always forgets about the important part - carrying.
+   //     Given two integers, find the result which the little boy will get.
+   //     Example
+   //     For param1 = 456 and param2 = 1734, the output should be 1180
+
+   // 456
+   //1734
+   // + ____
+   //1180
+    }
+}
